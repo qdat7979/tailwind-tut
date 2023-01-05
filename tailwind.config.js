dd7f65990abcdcdc9dfbc104c4ed58c74ad27f45
qdat7979 <qdat7979@gmail.com>
2023-01-04 16:54:22 -0800

    finish web

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./build/*.html",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens : {
+        'widescreen': {'raw': '(min-aspect-ratio: 3/2)'},
+        'tallscreen': {'raw': '(min-aspect-ratio: 1/2)'},
+      }
+    },
   },
   plugins: [],
 }
